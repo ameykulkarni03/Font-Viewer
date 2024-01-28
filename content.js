3ce4ec4b597b18b8d2a995944c31f3340a26fce8
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2024-01-29 04:03:22 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,7 @@
-// content.js
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.action === "changeFont") {
-    document.body.style.fontFamily = request.font;
+chrome.runtime.onMessage.addListener(function(request) {
+  if(request.font) {
+      document.body.style.fontFamily = request.font;
+  } else if(request.reset) {
+      document.body.style.fontFamily = '';
   }
 });
-  
-function resetFont() {
-  document.body.style.fontFamily = '';
-}
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.action === "resetFont") {
-    resetFont();
-  }
-});
-  
